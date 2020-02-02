3bcc960f6bc5f72012f1d3034db692df35cadb8c
Mostafa Abo El-Azayem <Kenany@localhost.localdomain>
2020-02-03 00:13:18 +0200

    -Added a prototype for insert function

---
+++ insertFunction.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+
+shopt -s extglob;
+
+function insert {
+    row="";
+    for col in `awk '{print $1}' table.metadata`
+    do
+        columnName=$(echo $col|cut -d ':' -f 1) ;
+        columnType=$(echo $col|cut -d ':' -f 2);
+        read -p "Enter $columnName value: " val;
+
+        #validation for string datatype
+        if test $columnType = "string"
+        then
+            while [ -z $val ] || [ "$val" -eq "$val" ] 2>/dev/null;
+            do
+              echo "invalid datatype!, $columnName column datatype is string";
+              read -p "Enter $columnName value: " val;
+            done
+        fi
+
+        #validation for integer datatype
+        if test $columnType = "integer"
+        then
+            while ! [ "$val" -eq "$val" ] 2>/dev/null;
+            do
+              echo "invalid datatype!, $columnName column datatype is integer";
+              read -p "Enter $columnName value: " val;
+            done
+        fi
+
+        #appending row to table with , as
+        if test -z $row
+        then
+            row=$val
+        else
+            row=$row","$val;
+        fi
+    done
+    echo "$row" >> table.data;
+}
+
+insert
