c4c7691be6b3af95e3a30c42d1f06bb59b29c868
Mostafa Abo El-Azayem <Kenany@localhost.localdomain>
2020-02-07 19:59:41 +0200

    -Formated file dbmsMainMenu.sh

--- dbmsMainMenu.sh
+++ dbmsMainMenu.sh
@@ -3,12 +3,75 @@ shopt -s extglob;
 
 . tableMenu.sh 
 
- echo "myDB DBMS is running";   
- mkdir -p ~/myDB/Databases ;
- cd ~/myDB/Databases ; 
 
+function listDB {
+ if [ -z "$(ls)" ]
+   then
+      echo "No databases avilable";
+      break;
+   else
+     echo "     ***************Available database(s)***************" ;
+     printf "\n";
+     ls;
+     printf "\n";
+ fi
+}
+##########################################
+function connectToDB {
+  read -p "Select Database you want to connect to: " db;
+  printf "\n";
+  if [ -d $db ]
+  then
+    cd $db;
+  echo "     ***************Now connected to Database $db***************";
+    printf "\n";
+    tableOptions;
+  else
+    echo " Sorry, Database $db not found!!";
+    printf "\n"
+  fi 
+}
+###########################################
+function drop {
+    read -p "Select Database you want to delete: " db;
+    printf "\n"
+    if [ -d $db ]
+    then
+      rm -r $db;
+      echo "     ***************Database $db deleted successfully***************"; 
+      printf "\n"
+    else
+        echo " Sorry, Database $db not found!!";
+        printf "\n"
+    fi
+}
+###########################################
+function createDB {
+  read -p "Enter database name:" db;
+  printf "\n";
+  if [[ ! $db  =~ ^[a-zA-Z_]+[a-zA-Z]+[0-9a-zA-Z_]*$ ]]; 
+  then
+    echo " Sorry, Invalid format!!";
+    printf "\n";
+  else
+    if [ -d $db ]
+    then
+        echo " Database already exist!!";
+        printf "\n";
+    else
+        mkdir $db;
+        echo "     ***************Database created successfully***************";
+        printf "\n";
+    fi
+  fi
+}
 
- clear;
+###########################################
+mkdir -p ~/myDB/Databases ;
+cd ~/myDB/Databases ; 
+clear;
+echo "myDB DBMS is running";   
+###########################################
  while true
  do
  printf "\n";
@@ -17,80 +80,39 @@ shopt -s extglob;
  do
   case $choice in
    "Create Database")
-    clear ;
-    read -p "Enter database name:" db;
-    printf "\n";
-    if [[ ! $db  =~ ^[a-zA-Z_]+[a-zA-Z]+[0-9a-zA-Z_]*$ ]]; 
-    then
-    echo " Sorry, Invalid format!!";
-    printf "\n";
-    else
-    if [ -d $db ]
-    then
-     echo " Database already exist!!";
-     printf "\n";
-    else
-    mkdir $db;
-    echo "     ***************Database created successfully***************";
-    printf "\n";
-    fi
-    fi
-    break;
+      clear ;
+      createDB;    
+      break;
     ;;
+
+##############################################
    "Show Databases")
-   clear ;
-   echo "     ***************Available database(s)***************" ;
-   printf "\n";
-   ls;
-   printf "\n";
-   break;
-   ;;
+     clear ;
+     listDB;
+     break;
+     ;;
+
+##############################################
    "Connect to a Database")
-   clear ;
-   echo "     ***************Available database(s)***************" ;
-   printf "\n";
-   ls;
-   printf "\n";
-   read -p "Select Database you want to connect to: " db;
-   printf "\n";
-   if [ -d $db ]
-   then
-    cd $db;
-    echo "     ***************Now connected to Database $db***************";
-    printf "\n";
-    tableOptions;
-   else
-    echo " Sorry, Database $db not found!!";
-    printf "\n"
-   fi 
-   break;
-   ;;
+     clear ;
+     listDB;
+     connectToDB;
+     break;
+     ;;
 
-
+##############################################
    "Drop Database")
-   clear ;
-   echo "     ***************Available database(s)***************" ;
-   printf "\n"
-   ls;
-   printf "\n"
-   read -p "Select Database you want to delete: " db;
-   printf "\n"
-   if [ -d $db ]
-   then
-    rm -r $db;
-    echo "     ***************Database $db deleted successfully***************"; 
-    printf "\n"
-   else
-    echo " Sorry, Database $db not found!!";
-    printf "\n"
-   fi 
-   break;
-   ;; 
+     clear ;
+     listDB;
+     drop;     
+     break;
+     ;; 
 
+##############################################
    "Exit")
-    cd ~ ;
-    break 2;
-    ;;
+      cd ~ ;
+      break 2;
+      ;;
   esac
  done
  done
